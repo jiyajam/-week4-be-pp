e74d3962980cbf338e7f4bac15b7c0d852fa53b9
Hoang-Vuu <Hoang.Vu3@metropolia.fi>
2025-11-14 18:08:59 +0200

    create userModel.js and userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,80 @@
+// controllers/userController.js
+const User = require('../models/userModel')
+
+// GET /users
+const getAllUsers = (req, res) => {
+  res.json(User.getAll())
+}
+
+// POST /users
+const createUser = (req, res) => {
+  const {
+    name,
+    email,
+    password,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status,
+    account_verified,
+    country,
+  } = req.body
+  const newUser = User.addOne(
+    name,
+    email,
+    password,
+    phone_number,
+    gender,
+    date_of_birth,
+    membership_status,
+    account_verified,
+    country,
+  )
+
+  if (newUser) {
+    res.status(201).json(newUser) // 201 Created
+  } else {
+    res
+      .status(400)
+      .json({ message: 'Failed to create user. Missing required fields.' })
+  }
+}
+
+const getUserById = (req, res) => {
+  const userId = req.params.userId
+  const user = User.findById(userId)
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).json({ message: 'User not found' })
+  }
+}
+
+const updateUser = (req, res) => {
+  const userId = req.params.tourId
+  const updatedData = req.body
+  const updatedUser = User.updateOneById(userId, updatedData)
+  if (updatedUser) {
+    res.json(updatedUser)
+  } else {
+    res.status(404).json({ message: 'User not found' })
+  }
+}
+
+const deleteUser = (req, res) => {
+  const userId = req.params.userId
+  const isDeleted = User.deleteOneById(userId)
+  if (isDeleted) {
+    res.status(204).send();
+  } else {
+    res.status(404).json({ message: 'User not found' })
+  }
+}
+
+module.exports = {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+}
---
+++ models/userModel.js
@@ -0,0 +1,84 @@
+let userArray = []
+let nextId = 1
+
+// Get all users
+const getAll = () => userArray
+
+// Add a new user
+const addOne = (
+  name,
+  email,
+  password,
+  phone_number,
+  gender,
+  date_of_birth,
+  membership_status,
+  account_verified,
+  country,
+) => {
+  if (
+    !name ||
+    !email ||
+    !password ||
+    !phone_number ||
+    !gender ||
+    !date_of_birth ||
+    !account_verified ||
+    !country
+  ) {
+    return false
+  }
+
+  const newUser = {
+    id: nextId++,
+    name,
+    email,
+    password,
+    phone_number,
+    gender,
+    date_of_birth,
+    account_verified,
+    country,
+  }
+
+  userArray.push(newUser)
+  return newUser
+}
+
+// Find user by ID
+const findById = (id) => {
+  const user = userArray.find((user) => user.id === Number(id))
+  return user || false
+}
+
+// Update user by ID
+const updateOneById = (id, updatedData) => {
+  const user = findById(id)
+  if (!user) return false
+
+  // Only update fields that exist in updatedData
+  Object.keys(updatedData).forEach((key) => {
+    if (updatedData[key] !== undefined) {
+      tour[key] = updatedData[key]
+    }
+  })
+
+  return user
+}
+
+// Delete user by ID
+const deleteOneById = (id) => {
+  const user = findById(id)
+  if (!user) return false
+
+  userArray = userArray.filter((user) => user.id !== Number(id))
+  return true
+}
+
+module.exports = {
+  getAll,
+  addOne,
+  findById,
+  updateOneById,
+  deleteOneById,
+}
